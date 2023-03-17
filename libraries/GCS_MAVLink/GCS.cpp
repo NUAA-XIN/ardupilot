f3e796660f07f70274078faa164b8717ab04082f
NUAA-XIN <127838873+NUAA-XIN@users.noreply.github.com>
2023-03-17 15:26:22 +0800

    1Hz发送高度到地面站

--- libraries/GCS_MAVLink/GCS.cpp
+++ libraries/GCS_MAVLink/GCS.cpp
@@ -12,7 +12,7 @@ void GCS::send_textv(MAV_SEVERITY severity, const char *fmt, va_list arg_list)
     send_statustext(severity, GCS_MAVLINK::active_channel_mask() | GCS_MAVLINK::streaming_channel_mask(), text);
 }
 
-void GCS::send_text(MAV_SEVERITY severity, const char *fmt, ...)
+void GCS::send_text(MAV_SEVERITY severity, const char *fmt, ...)   //向地面站发送消息
 {
     va_list arg_list;
     va_start(arg_list, fmt);
